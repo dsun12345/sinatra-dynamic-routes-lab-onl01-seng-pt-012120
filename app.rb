ae49db54da2c1a9b09592d70d69306ebee713c05
Daniel Sun <danielsun12345@gmail.com>
2020-04-15 19:35:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -35,8 +35,8 @@ class App < Sinatra::Base
    operation = params[:operation]
    number1 = params[:number1].to_i
    number2 = params[:number2].to_i
-   case
-   when operation == "add"
+   case operation
+   when "add"
      "#{@Number1} + #{@number2}"
    elsif @operation == "subtract"
      "#{@number1} - #{@number2}"
